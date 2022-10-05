1ab3786102045ec4b572056ea69c7fce0ba595f5
MurilowJr <murilobrito.jr@gmail.com>
2022-10-05 15:24:14 -0300

    Update CurrencyConverter.java

--- src/util/CurrencyConverter.java
+++ src/util/CurrencyConverter.java
@@ -3,12 +3,11 @@ package util;
 public class CurrencyConverter {
 	
 	//classes declaradas com "final" são classes imutáveis, ou seja, não alteram seu valor.
-	public static final double IOF = 6.0;
+	public static final double IOF = 0.06;
 	
 	public static double converter(double dollar, double quantity) {
 		
-		double valor = dollar * quantity;
-		return valor += valor * IOF/100;
+		return dollar * quantity * (1.0 + IOF);
 		
 	}
 	
